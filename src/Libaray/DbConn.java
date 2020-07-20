885ab2670d8d56016f2ae0cde2bf246bb012f988
Jaffar Abbas <gamaportal8@gmail.com>
2020-07-20 22:29:00 +0500

    20-7-2020

--- src/Libaray/DbConn.java
+++ src/Libaray/DbConn.java
@@ -7,25 +7,68 @@ import java.sql.DriverManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DbConn implements MainConnection {
+public class DbConn{
 
     public Connection connection;
     public Statement statement;
     public ResultSet result;
 
-    @Override
-    public void Connection() {
+
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/login?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String DATABASE_USERNAME = "root";
+    private static final String DATABASE_PASSWORD = "";
+    private static final String SELECT_QUERY_LOGIN = "SELECT * FROM login WHERE user = ? and password = ?";
+
+
+    public static String UserId;
+
+    public DbConn() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+            connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
             statement = connection.createStatement();
         } catch (ClassNotFoundException | SQLException e) {
-            Logger.getLogger(DbConn.class.getName()).log(Level.SEVERE,null,e);
+              e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        DbConn obj= new DbConn();
-        obj.Connection();
+    public boolean validate(String emailId, String password) throws SQLException {
+
+        // Step 1: Establishing a Connection and
+        // try-with-resource statement will auto close the connection.
+        try (// Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY_LOGIN)) {
+            preparedStatement.setString(1, emailId);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                UserId = emailId;
+                return true;
+            }
+
+        } catch (SQLException e) {
+            // print SQL exception information
+            printSQLException(e);
+        }
+        return false;
+    }
+
+
+    //print the error
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
     }
 }
--- src/Libaray/Login_Controller.java
+++ src/Libaray/Login_Controller.java
@@ -2,19 +2,37 @@ package Libaray;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import javafx.stage.Window;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class Login_Controller {
+
     public TextField username;
     public PasswordField password;
     public Button submitbutton;
+
+
+    public AnchorPane Panel;
+    public AnchorPane login_page;
 
 
     //minimizw
@@ -27,41 +45,79 @@ public class Login_Controller {
         System.exit(1);
     }
 
-    public void login(ActionEvent event) throws SQLException, IOException,Exception {
+
+    public void submitbutton(ActionEvent event) throws SQLException, IOException,Exception {
 
 
         Window owner = submitbutton.getScene().getWindow();
 
         if(username.getText().isEmpty()) {
-            showAlert(AlertType.ERROR, owner, "Form Error!",
-                    "Please enter your email id");
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your Username");
             return;
         }
-        if(passwordField.getText().isEmpty()) {
-            showAlert(AlertType.ERROR, owner, "Form Error!",
-                    "Please enter a password");
+        if(password.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your Password");
             return;
+
+
+                String user = username.getText();
+                String passwords = password.getText();
+
+                DbConn jdb = new DbConn();
+                boolean flag = jdb.validate(user, password);
+
+
+
+
+                if(!flag) {
+                    infoBox("Please enter correct Email and Password", null, "Failed");
+                }else {
+                    infoBox("Login Successful!", null, "Failed");
+
+                    Student_plane.setOpacity(100);
+                    Student_plane.setDisable(false);
+                    Login_plane.setOpacity(0);
+                    Login_plane.setDisable(true);
+                    UCategoryAxis.setText(jdbcDao.getUserId());
+                    Chart();
+
+
+                }
+            }
+
+
+            Panel.setOpacity(100);
+            Panel.setDisable(false);
+            login_page.setOpacity(0);
+            login_page.setDisable(true);
         }
+    }
 
-        String emailId = .getText();
-        String password = passwordField.getText();
+//
+//    public void displayDashborad(ActionEvent event) {
+//        Panel.setOpacity(100);
+//        Panel.setDisable(false);
+//        login_page.setOpacity(0);
+//        login_page.setDisable(true);
+//    }
 
-        jdbcDao jdb = new jdbcDao();
-        boolean flag = jdb.validate(emailId, password);
+    private static void infoBox(String infoMessage, String headerText, String title){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
 
-        if(!flag) {
-            infoBox("Please enter correct Email and Password", null, "Failed");
-        }else {
-            infoBox("Login Successful!", null, "Failed");
-            Student_plane.setOpacity(100);
-            Student_plane.setDisable(false);
-            Login_plane.setOpacity(0);
-            Login_plane.setDisable(true);
-            UCategoryAxis.setText(jdbcDao.getUserId());
-            Chart();
-
-
-        }
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
     }
 
 }
